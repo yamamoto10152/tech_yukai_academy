f7fde6605fee4bc41e834443b62a6ade65729057
あなたの名前 <yamada10069@ashisuto.co.jp>
2026-07-29 06:57:56 +0000

    更新

--- training/Day2_AM/02_文字列と入力値変換/extra/01_文字列の基本.py
+++ training/Day2_AM/02_文字列と入力値変換/extra/01_文字列の基本.py
@@ -1,12 +1,37 @@
 """
 Day2: 文字列の基本（追加問題・任意）
-実行: python training/day02_am/文字列と入力値変換/extra/01_文字列の基本.py
+実行: python training/Day2_AM/02_文字列と入力値変換/extra/01_文字列の基本.py
+参考: training/Day2_AM/02_文字列と入力値変換/solution/01_文字列の基本.py
 """
 
-# "error" を3回繰り返し、先頭に "API " を + でつないで print してみよう
+# 【基本】
+
+# "Python" と "研修" を + でつなげて、printで表示してみよう
 
 
-# 変数 s に "password" を入れ、len(s) が 8 以上かどうかを len(s) >= 8 の結果で print してみよう
+# "ab" を 4回繰り返して、printで表示してみよう
 
 
-# シングルクォートとダブルクォートで同じ文字列を2通り print してみよう
+# len("Hello") の結果を、printで表示してみよう
+
+
+# 【普通】
+
+# len("山田") の結果を、printで表示してみよう
+
+
+# 変数 label に "sample" を入れ、len(label) を print してみよう
+
+
+# "API" と "Client" を + でつないで、printで表示してみよう
+
+
+# 【難しい】
+
+# "Ha" を 5回繰り返した結果を、printで表示してみよう
+
+
+# 変数 prefix に "user_"、suffix に "001" を入れ、+ でつないで print してみよう
+
+
+# 変数 text に任意の文字列を入れ、text の長さを len() で print してみよう
